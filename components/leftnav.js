e7bb85833f183a3645c5f8e2d89d66db2b81696e
q82717182q <q82717182q@gmail.com>
2023-08-15 14:31:17 -0700

    修改Sidebar，固定在左邊，改成長條狀

--- components/leftnav.js
+++ components/leftnav.js
@@ -1,43 +1,40 @@
 import Link from "next/link";
 import {useRouter} from 'next/router';
 
-
 export default function LeftNav({children}) {
     const router = useRouter();
     return (
-        <div className="container mt-5">
-            <div className="row">
-                {/* 左側導航列 */}
-                <div className="col-md-2">
-                    <nav className="nav flex-column">
-                        <Link href="/doctormanagement"
-                              className={`btn btn-outline-primary  ${router.pathname === "/doctormanagement" && "btn-primary active"}`}>
-                            醫生管理
-                        </Link>
-                        <Link href="/pdfmanagement"
-                              className={`btn btn-outline-primary ${router.pathname === "/pdfmanagement" && "btn-primary active"}`}>
-                            PDF管理
-                        </Link>
-                        <Link href="/accountmanagement"
-                              className={`btn btn-outline-primary ${router.pathname === "/accountmanagement" && "btn-primary active"}`}>
-                            帳號管理
-                        </Link>
-                        <Link href="/rolemanagement"
-                              className={`btn btn-outline-primary ${router.pathname === "/rolemanagement" && "btn-primary active"}`}>
-                            角色管理
-                        </Link>
-                        <Link href="/report"
-                              className={`btn btn-outline-primary ${router.pathname === "/report" && "btn-primary active"}`}>
-                            報告
-                        </Link>
-                    </nav>
-                </div>
+            <div className="d-flex">
+                    {/* 左側導航列 */}
+                    <div className="col-md-2 bg-primary sticky-top vh-100">
+                        <nav className="nav flex-column">
+                            <Link href="/doctormanagement"
+                                  className={`btn nav-link text-light m-2 ${router.pathname === "/doctormanagement" && "btn-light active"}`}>
+                                醫生管理
+                            </Link>
+                            <Link href="/pdfmanagement"
+                                  className={`btn nav-link text-light m-2 ${router.pathname === "/pdfmanagement" && "btn-light active"}`}>
+                                PDF管理
+                            </Link>
+                            <Link href="/accountmanagement"
+                                  className={`btn nav-link text-light m-2 ${router.pathname === "/accountmanagement" && "btn-light active"}`}>
+                                帳號管理
+                            </Link>
+                            <Link href="/rolemanagement"
+                                  className={`btn nav-link text-light m-2 ${router.pathname === "/rolemanagement" && "btn-light active"}`}>
+                                角色管理
+                            </Link>
+                            <Link href="/report"
+                                  className={`btn nav-link text-light m-2 ${router.pathname === "/report" && "btn-light active"}`}>
+                                報告
+                            </Link>
+                        </nav>
+                    </div>
 
-                {/* 右側內容呈現區塊 */}
-                <div className="col-md-10">
-                    {children}
-                </div>
+                    {/* 右側內容呈現區塊 */}
+                    <div className="flex-grow-1 p-3 ml-500 overflow-auto">
+                        {children}
+                    </div>
             </div>
-        </div>
     );
 };
